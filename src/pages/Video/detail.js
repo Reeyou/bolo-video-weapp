c5a6dc4de5cd6f6975ec8341551d06b7e6842d60
Reeyou <ryu993830137@163.com>
2019-07-03 18:31:33 +0800

    添加视频主页内容

---
+++ src/pages/Video/detail.js
@@ -0,0 +1,24 @@
+import Taro , { Component } from '@tarojs/taro';
+import { View, Text , Button, Form, Input} from '@tarojs/components';
+
+export default class VideoInfo extends Component {
+
+   config = {
+       navigationBarTitleText: ''
+  }
+
+  state={}
+
+  componentWillMount () {}
+  componentDidMount () {} 
+  componentWillReceiveProps (nextProps,nextContext) {} 
+  componentWillUnmount () {} 
+
+  render() {
+    return (
+      <View className='detail'>
+        
+      </View>
+    );
+  }
+}
--- src/pages/Video/index.js
+++ src/pages/Video/index.js
@@ -1,5 +1,6 @@
 import Taro , { Component } from '@tarojs/taro';
 import { View, Text , Button, Form, Input} from '@tarojs/components';
+import './index.styl';
 
 export default class VideoInfo extends Component {
 
@@ -9,15 +10,25 @@ export default class VideoInfo extends Component {
 
   state={}
 
-  componentWillMount () {}
-  componentDidMount () {} 
-  componentWillReceiveProps (nextProps,nextContext) {} 
-  componentWillUnmount () {} 
-
   render() {
     return (
-      <View className='info'>
-        
+      <View className='video'>
+        <View className='wrapper'>
+          <View className='container'>
+
+          </View>
+          <View className='footer'>
+            <View className='left'>
+              <Text className='avatar'></Text>
+            </View>
+            <View className='right'>
+              <View>
+                <Text></Text>
+                <Text>5.1万</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
