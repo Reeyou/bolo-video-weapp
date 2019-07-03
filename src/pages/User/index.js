470ab33cc4c5d88bfe05f7dcf8eb100607776720
Reeyou <ryu993830137@163.com>
2019-07-03 18:12:50 +0800

    用户页面样式优化完成

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,15 +3,17 @@ import { View, Text , Button, Form, Input, Label} from '@tarojs/components';
 import "./index.styl"
 
 export default class User extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fansCounts: 0,
+      followCounts: 0,
+      receiveLikeCounts: 0,
+    }
+  }
 
    config = {
        navigationBarTitleText: ''
-  }
-
-  state={
-    fansCounts: 0,
-    followCounts: 0,
-    receiveLikeCounts: 0,
   }
 
   componentWillMount () {}
@@ -25,34 +27,32 @@ export default class User extends Component {
           <View className='info'>
             <View className='avatar'></View>
             <View className='btn'>
-              <Button type='primary'>上传作品</Button>
-              <Button type='primary'>注销</Button>
+              <Button size='mini' type='primary'>上传作品</Button>
+              <Button size='mini' type='primary'>注销</Button>
             </View>
             <View className='followBtn'>
-              <Button type='primary'>关注我</Button>
+              <Button size='mini' type='primary'>关注我</Button>
             </View>
-            <View className='tab'>
-              <Label class='info-items'>{{fansCounts}} 粉丝</Label>
-              <Label class='info-items'>{{followCounts}} 关注</Label>
-              <Label class='info-items'>{{receiveLikeCounts}} 获赞</Label>
+            <View className='count'>
+              <Label class='info-items'>{fansCounts} 粉丝</Label>
+              <Label class='info-items'>{followCounts} 关注</Label>
+              <Label class='info-items'>{receiveLikeCounts} 获赞</Label>
             </View>
           </View>
-          <View className='mine'>
-            <View className='btnTab'>
-              <Label class='tab active' bindtap='doSelectWork'>作品</Label>
-              <Label class='tab' bindtap='doSelectLike'>收藏</Label>
-              <Label class='tab' bindtap='doSelectFollow'>关注</Label>
+          <View className='tab'>
+            <Label class='tablist tablist_left active' bindtap='doSelectWork'>作品</Label>
+            <Label class='tablist tablist_mid' bindtap='doSelectLike'>收藏</Label>
+            <Label class='tablist tablist_right' bindtap='doSelectFollow'>关注</Label>
+          </View>
+          <View classname='list'>
+            <View classname='item work'>
+              <Image src=''/>
             </View>
-            <View classname='list'>
-              <View classname='item work'>
-                <Image src=''/>
-              </View>
-              <View classname='item like'>
-                <Image src=''/>
-              </View>
-              <View classname='item follow'>
-                <Image src=''/>
-              </View>
+            <View classname='item like'>
+              <Image src=''/>
+            </View>
+            <View classname='item follow'>
+              <Image src=''/>
             </View>
           </View>
         </View>
